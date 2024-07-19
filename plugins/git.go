41e646219e05637498b8d38724bd2dc5dd525034
John Keck <robertjkeck2@gmail.com>
2024-07-18 22:14:33 -0500

    feat: git plugin initial commit

--- plugins/git.go
+++ plugins/git.go
@@ -1,17 +1,126 @@
 package plugins
 
-type Git struct{}
+import (
+	"errors"
+	"os/exec"
+	"strings"
+)
 
-func (g *Git) New(upstream string) {}
+type Git struct {
+	Remote string
+}
 
-func (g *Git) Add(file string) {}
+func (g *Git) New() {
+	_, err := exec.LookPath("git")
+	if err != nil {
+		panic("git is not installed")
+	}
 
-func (g *Git) AddAll() {}
+	// Get the remote URL
+	remote, err := exec.Command("git", "config", "--get", "remote.origin.url").Output()
+	if err != nil {
+		panic("unable to get remote URL")
+	}
 
-func (g *Git) Commit(message string) {}
+	g.Remote = string(remote)
 
-func (g *Git) Push() {}
+	branch, err := g.Branch()
+	if err != nil {
+		panic("unable to get current branch")
+	}
 
-func (g *Git) Pull() {}
+	// Pull the latest changes
+	_, err = exec.Command("git", "pull", "origin", branch).Output()
+	if err != nil {
+		panic("unable to pull latest changes")
+	}
+}
 
-func (g *Git) Checkout(branch string) {}
+func (g *Git) Status() (string, error) {
+	status, err := exec.Command("git", "status").Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(status), nil
+}
+
+func (g *Git) Add(file string) error {
+	_, err := exec.Command("git", "add", file).Output()
+	if err != nil {
+		return errors.New("unable to add file: " + file)
+	}
+
+	return nil
+}
+
+func (g *Git) AddAll() error {
+	_, err := exec.Command("git", "add", ".").Output()
+	if err != nil {
+		return errors.New("unable to add all files")
+	}
+
+	return nil
+}
+
+func (g *Git) Branch() (string, error) {
+	branch, err := exec.Command("git", "branch").Output()
+	if err != nil {
+		return "", errors.New("unable to get current branch")
+	}
+
+	lines := strings.Split(string(branch), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "*") {
+			return strings.TrimSpace(strings.TrimPrefix(line, "* ")), nil
+		}
+	}
+
+	return "", nil
+}
+
+func (g *Git) Commit(message string) error {
+	_, err := exec.Command("git", "commit", "-m", message).Output()
+	if err != nil {
+		return errors.New("unable to commit changes")
+	}
+
+	return nil
+}
+
+func (g *Git) PushCurrentBranch() error {
+	branch, err := g.Branch()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("git", "push", "origin", branch).Output()
+	if err != nil {
+		return errors.New("unable to push changes to branch: " + branch)
+	}
+
+	return nil
+}
+
+func (g *Git) PullCurrentBranch() error {
+	branch, err := g.Branch()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("git", "pull", "origin", branch).Output()
+	if err != nil {
+		return errors.New("unable to pull changes from branch: " + branch)
+	}
+
+	return nil
+}
+
+func (g *Git) Checkout(branch string) error {
+	_, err := exec.Command("git", "checkout", branch).Output()
+	if err != nil {
+		return errors.New("unable to checkout branch: " + branch)
+	}
+
+	return nil
+}
